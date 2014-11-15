732301d0958877b4efa7c06792b16b0c79841d28
StarSheep <Vetrogon2000@gmail.com>
2014-11-15 22:36:26 +0500

    Moved the dividing to ForgeHooksClient
    because doing it inside the event was a bad idea

--- src/main/java/net/minecraftforge/client/ForgeHooksClient.java
+++ src/main/java/net/minecraftforge/client/ForgeHooksClient.java
@@ -410,7 +410,13 @@ public class ForgeHooksClient
             }
         }
 
-        int multiplier = (r / divider & 255) << 16 | (g / divider & 255) << 8 | b / divider & 255;
+        SkyColorEvent event = new SkyColorEvent(r / divider, g / divider, b / divider);
+        MinecraftForge.EVENT_BUS.post(event);
+        r = event.red;
+        g = event.green;
+        b = event.blue;
+        
+        int multiplier = (r & 255) << 16 | (g & 255) << 8 | b & 255;
 
         skyX = playerX;
         skyZ = playerZ;
